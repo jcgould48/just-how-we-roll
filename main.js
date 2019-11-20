13674e2f8921c3bd8da49c5cfcf3f0ea45432a35
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-20 10:11:20 -0500

    adds other dice

--- main.js
+++ main.js
@@ -47,18 +47,59 @@ function median(arr) {
     return median;
 }
 
+function mode(arr){
+
+}
 
 
 
  document.querySelector('#d6-roll').src= "images/start/d6.png"
 
  function rollSix(){
-    let rollSixResult = getRandomNumber(6);
-    document.querySelector('#d6-roll').src = `images/d6/${rollSixResult}.png`
-    sixes.push(rollSixResult)
+    let rollResult = getRandomNumber(6);
+    document.querySelector('#d6-roll').src = `images/d6/${rollResult}.png`
+    sixes.push(rollResult)
     console.log(sixes)
     document.querySelector('#d6-rolls-mean').innerText = mean(sixes);
     document.querySelector('#d6-rolls-median').innerText = median(sixes)
+    document.querySelector('#d6-rolls-mode').innerText = mode(sixes)
+}
+
+document.querySelector('#double-d6-roll-1').src= "images/start/d6.png"
+document.querySelector('#double-d6-roll-2').src= "images/start/d6.png"
+
+ function rollDouble(){
+    let rollResult = getRandomNumber(6);
+    document.querySelector('#d6-roll').src = `images/d6/${rollResult}.png`
+    sixes.push(rollResult)
+    console.log(sixes)
+    document.querySelector('#d6-rolls-mean').innerText = mean(sixes);
+    document.querySelector('#d6-rolls-median').innerText = median(sixes)
+    document.querySelector('#d6-rolls-mode').innerText = mode(sixes)
+}
+
+document.querySelector('#d12-roll').src= "images/start/d12.png"
+
+ function rollTwelve(){
+    let rollResult = getRandomNumber(12);
+    document.querySelector('#d12-roll').src = `images/d12/${rollResult}.png`
+    twelves.push(rollResult)
+    console.log(twelves)
+    document.querySelector('#d12-rolls-mean').innerText = mean(twelves);
+    document.querySelector('#d12-rolls-median').innerText = median(twelves)
+    document.querySelector('#d12-rolls-mode').innerText = mode(twelves)
+}
+
+document.querySelector('#d20-roll').src= "images/start/d20.png"
+
+ function rollTwenty(){
+    let rollResult = getRandomNumber(20);
+    document.querySelector('#d20-roll').src = `images/d20/${rollResult}.png`
+    twenties.push(rollResult)
+    console.log(twenties)
+    document.querySelector('#d20-rolls-mean').innerText = mean(twenties);
+    document.querySelector('#d20-rolls-median').innerText = median(twenties)
+    document.querySelector('#d20-rolls-mode').innerText = mode(twenties)
 }
 
 
@@ -67,9 +108,9 @@ function median(arr) {
  * EVENT LISTENERS *
  *******************/
 document.querySelector("#d6-button").addEventListener("click", rollSix);
-// document.querySelector("#double-d6").addEventListener("click", rollDouble);
-// document.querySelector("#d12").addEventListener("click", rollTwelve);
-// document.querySelector("#d20").addEventListener("click", rollTwenty);
+document.querySelector("#double-d6-buttons").addEventListener("click", rollDouble);
+document.querySelector("#d12-button").addEventListener("click", rollTwelve);
+document.querySelector("#d20-button").addEventListener("click", rollTwenty);
 
 /****************
  * MATH SECTION *
